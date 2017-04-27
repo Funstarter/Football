6d6d32cc04b39c9ef6886c5c5dd153eec9d969be
Kepler-22b <andriy@siteimage.com.ua>
2017-04-27 11:58:45 +0300

    Start create multigames. Create click handler on table and delegate it to game controller

--- src/js/app.js
+++ src/js/app.js
@@ -5,11 +5,39 @@ var games = [
   }
 ];
 
-document.querySelector('[data-game-control]').addEventListener('click', function (e) {
-  var gameBoard = new Board (games[0].homeTeam, games[0].awayTeam);
+var teams = [
+  {
+    'name': 'Arsenal',
+    'power': 60
+  },
+  {
+    'name': 'MC',
+    'power': 90
+  },
+  {
+    'name': 'Chelsea',
+    'power': 80
+  },
+  {
+    'name': 'MU',
+    'power': 85
+  }
+]
+
+document.querySelector('[data-table]').addEventListener('click', function (e) {
+
+  /* Delegate handler */
+  var control = e.target;
+  while (!control.hasAttribute('data-game-control')) {
+    control = control.parentNode;
+  }
+
+  console.log(control);
+
+  /*var gameBoard = new Board (games[0].homeTeam, games[0].awayTeam);
   var game = new Game(games[0].homeTeam, games[0].awayTeam);
   game.subscribe('scoreGoal', gameBoard.showScore);
   game.subscribe('showTime', gameBoard.showTime);
   game.subscribe('message', gameBoard.showSummary);
-  game.play();
+  game.play();*/
 });
