7b1208bcd6f6d90b337f9ecd5e3e4063856f6b7a
Kepler-22b <andriy@siteimage.com.ua>
2017-05-09 14:09:06 +0300

    Play game from list on which you have clicked

--- src/js/app.js
+++ src/js/app.js
@@ -2,42 +2,26 @@ var games = [
   {
     homeTeam: new Team('Arsenal', 60),
     awayTeam: new Team('MC', 90)
+  },
+  {
+    homeTeam: new Team('Chelsea', 75),
+    awayTeam: new Team('MU', 80)
   }
 ];
-
-var teams = [
-  {
-    'name': 'Arsenal',
-    'power': 60
-  },
-  {
-    'name': 'MC',
-    'power': 90
-  },
-  {
-    'name': 'Chelsea',
-    'power': 80
-  },
-  {
-    'name': 'MU',
-    'power': 85
-  }
-]
 
 document.querySelector('[data-table]').addEventListener('click', function (e) {
 
   /* Delegate handler */
   var control = e.target;
-  while (!control.hasAttribute('data-game-control')) {
+  while (!control.hasAttribute('data-game')) {
     control = control.parentNode;
   }
+  var game = parseInt(control.getAttribute('data-game'));
 
-  console.log(control);
-
-  /*var gameBoard = new Board (games[0].homeTeam, games[0].awayTeam);
-  var game = new Game(games[0].homeTeam, games[0].awayTeam);
+  var gameBoard = new Board (games[game].homeTeam, games[game].awayTeam);
+  var game = new Game(games[game].homeTeam, games[game].awayTeam);
   game.subscribe('scoreGoal', gameBoard.showScore);
   game.subscribe('showTime', gameBoard.showTime);
   game.subscribe('message', gameBoard.showSummary);
-  game.play();*/
+  game.play();
 });
--- src/js/board.js
+++ src/js/board.js
@@ -6,8 +6,11 @@ var Board = function (homeTeam, awayTeam) {
 }
 
 Board.prototype.render = function () {
+  this.board.querySelector('#score').innerHTML = '0:0';
   this.board.querySelector('#homeTeamName').innerHTML = this.homeTeam.name;
   this.board.querySelector('#awayTeam').innerHTML = this.awayTeam.name;
+  this.board.querySelector('#summary').innerHTML = '';
+  this.board.querySelector('#timer').innerHTML = '0';
 }
 
 Board.prototype.showScore = function (score) {
@@ -15,9 +18,9 @@ Board.prototype.showScore = function (score) {
 }
 
 Board.prototype.showTime = function (time) {
-  document.querySelector('.timer').innerHTML = time;
+  document.querySelector('#timer').innerHTML = time;
 }
 
 Board.prototype.showSummary = function (message) {
-  document.querySelector('.summary').insertAdjacentHTML('beforeEnd', '<p>' + message + '</p>');
+  document.querySelector('#summary').insertAdjacentHTML('beforeEnd', '<p>' + message + '</p>');
 }
