201cd6ecb42401dff4355002b9e8dc0c75d2bbf1
Kepler-22b <andriy@siteimage.com.ua>
2017-08-08 06:51:15 +0300

    Fix game variable rewrite

--- src/js/app.js
+++ src/js/app.js
@@ -16,10 +16,10 @@ document.querySelector('[data-table]').addEventListener('click', function (e) {
   while (!control.hasAttribute('data-game')) {
     control = control.parentNode;
   }
-  var game = parseInt(control.getAttribute('data-game'));
+  var gameId = parseInt(control.getAttribute('data-game'));
 
-  var gameBoard = new Board (games[game].homeTeam, games[game].awayTeam);
-  var game = new Game(games[game].homeTeam, games[game].awayTeam);
+  var gameBoard = new Board (games[gameId].homeTeam, games[gameId].awayTeam);
+  var game = new Game(games[gameId].homeTeam, games[gameId].awayTeam);
   game.subscribe('scoreGoal', gameBoard.showScore);
   game.subscribe('showTime', gameBoard.showTime);
   game.subscribe('message', gameBoard.showSummary);
