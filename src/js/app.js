6df6d7e7c6d16ba4a9d6fe14603c5fb416528b54
Kepler-22b <andriy@siteimage.com.ua>
2017-08-08 07:18:44 +0300

    Fix table play button handler. Made active only button area

--- src/js/app.js
+++ src/js/app.js
@@ -13,10 +13,22 @@ document.querySelector('[data-table]').addEventListener('click', function (e) {
 
   /* Delegate handler */
   var control = e.target;
-  while (!control.hasAttribute('data-game')) {
+  while (!control.hasAttribute('data-game-control')) {
+    control = control.parentNode;
+    if(control === e.currentTarget) {
+        return;
+    }
+  }
+
+  /* Get Game id */
+  var gameId;
+  while (control !== e.currentTarget) {
+    if (control.hasAttribute('data-game')) {
+      gameId = parseInt(control.getAttribute('data-game'));
+      break;
+    }
     control = control.parentNode;
   }
-  var gameId = parseInt(control.getAttribute('data-game'));
 
   var gameBoard = new Board (games[gameId].homeTeam, games[gameId].awayTeam);
   var game = new Game(games[gameId].homeTeam, games[gameId].awayTeam);
