4a457ba02e2bdad847a0a964c28b43e571f2d4ef
Kepler-22b <andriy@siteimage.com.ua>
2017-10-19 07:43:28 +0300

    Render teams with creating dom in js

--- src/js/app/match-center/match-center.js
+++ src/js/app/match-center/match-center.js
@@ -15,15 +15,11 @@
         },
         renderTeams: function () {
             this.teamsDropdown.forEach(function (dropdown) {
-                var template = dropdown.innerHTML;
-                var result = '';
-                var teamOption = '';
-                this.teams.forEach(function(team, index){
-                    teamOption = template.replace(/{(team.name)}/g, team.name);
-                    teamOption = teamOption.replace(/{(team.id)}/g, index+'');
-                    result += teamOption;
+                var options = '';
+                teams.forEach(function (team) {
+                    options += '<option value="'+ team.id +'">' + team.name + '</option>';
                 });
-                dropdown.innerHTML = result;
+                dropdown.insertAdjacentHTML('beforeEnd', options);
             });
         },
         addGame: function () {
